4a24347456cf5c617e5e107b2611c53d4e3f1246
Sukirtha2954 <69414332+Sukirtha2954@users.noreply.github.com>
2024-07-04 22:06:14 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,20 +1,11 @@
 from django.db import models
 
-
 class Review(models.Model):
-    SENTIMENT_CHOICES = (
-        ('Positive', 'Positive'),
-        ('Negative', 'Negative'),
-        ('Neutral', 'Neutral'),
-    )
-
-    title = models.CharField(max_length=200)
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
     content = models.TextField()
-    rating = models.FloatField()
-    sentiment = models.CharField(max_length=10, choices=SENTIMENT_CHOICES, null=True)
+    sentiment = models.CharField(max_length=20)
+    url = models.URLField(max_length=255, default='https://amazon.in')
 
     def __str__(self):
         return self.title
-
-
-#Create the proj in django,with reg,login,after login the user will inpit the url and click start,the system should start working,after that it should show the analytics in graphical and tabular format
